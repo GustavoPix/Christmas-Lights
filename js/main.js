e2e7283e0c833a6bf73f58a91f42cf4893651a4d
Gustavo Carvalho <gustavo1net@hotmail.com>
2020-05-03 11:21:42 -0300

    new input change color, itensity, size light and add line

--- js/main.js
+++ js/main.js
@@ -5,57 +5,45 @@ const vm_main = new Vue({
         timer:0,
         updateTime:10000,
         speed:100,
+        maxItensity:100,
         ligths:[
             {
-                r:215,
-                g:54,
-                b:36,
                 on:true,
                 itensity:1
             },
             {
-                r:238,
-                g:99,
-                b:42,
                 on:false,
                 itensity:0
             },
             {
-                r:244,
-                g:217,
-                b:66,
                 on:true,
                 itensity:1
             },
             {
-                r:23,
-                g:119,
-                b:73,
                 on:false,
                 itensity:0
             },
             {
-                r:33,
-                g:125,
-                b:212,
                 on:true,
                 itensity:1
             },
             {
-                r:38,
-                g:60,
-                b:159,
                 on:false,
                 itensity:0
             },
             {
-                r:110,
-                g:27,
-                b:95,
                 on:true,
                 itensity:1
             }
         ],
+        lightSelected:{
+            line:0,
+            light:0,
+            r:0,
+            g:0,
+            b:0,
+            size:0
+        },
         setup:{
             lines:[],
             lights:7
@@ -69,15 +57,19 @@ const vm_main = new Vue({
         start()
         {
             this.addLine();
+            this.selectLight(0,0);
         },
         addLine()
         {
-            this.setup.lines.push({
-                lights:[]
-            });
-            for(let i = 0; i < this.setup.lights; i++)
+            if(this.setup.lines.length < 8)
             {
-                this.addColor();
+                this.setup.lines.push({
+                    lights:[]
+                });
+                for(let i = 0; i < this.setup.lights; i++)
+                {
+                    this.addColor();
+                }
             }
         },
         addColor()
@@ -86,9 +78,19 @@ const vm_main = new Vue({
                 l.lights.push({
                     r:Math.floor(Math.random() * 255),
                     g:Math.floor(Math.random() * 255),
-                    b:Math.floor(Math.random() * 255)
+                    b:Math.floor(Math.random() * 255),
+                    size:50
                 });
             });
+        },
+        selectLight(line,light)
+        {
+            this.lightSelected.line = line;
+            this.lightSelected.light = light;
+            this.lightSelected.r = this.setup.lines[line].lights[light].r;
+            this.lightSelected.g = this.setup.lines[line].lights[light].g;
+            this.lightSelected.b = this.setup.lines[line].lights[light].b;
+            this.lightSelected.size = this.setup.lines[line].lights[light].size;
         },
         async updateFrame()
         {
@@ -113,12 +115,12 @@ const vm_main = new Vue({
                 if(l.on)
                 {
                     //l.itensity-=1/this.updateTime;
-                    l.itensity=this.timer/(this.updateTime/this.speed);
+                    l.itensity=this.timer/(this.updateTime/this.speed)*(this.maxItensity/100);
                 }
                 else
                 {
                     //l.itensity+=1/(this.updateTime/this.speed);
-                    l.itensity=1-this.timer/(this.updateTime/this.speed);
+                    l.itensity=(1*(this.maxItensity/100))-this.timer/(this.updateTime/this.speed);
                 }
             });
         },
@@ -132,6 +134,24 @@ const vm_main = new Vue({
         {
             return `radial-gradient(circle, rgba(${lights[i].r}, ${lights[i].g}, ${lights[i].b},${this.ligths[i].itensity}) ${30*this.ligths[i].itensity}%,  rgba(0,0,0,0) 70%, rgba(0,0,0,0) 100%)`;
         },
+        changeColor(color)
+        {
+            let light = this.setup.lines[this.lightSelected.line].lights[this.lightSelected.light];
+            this.lightSelected[color] = this.lightSelected[color].replace(" ","");
+            this.lightSelected[color] = this.lightSelected[color].replace(/[^0-9]/g,"");
+            this.lightSelected[color] = this.lightSelected[color] > 255 ? 255 : this.lightSelected[color];
+            this.lightSelected[color] = this.lightSelected[color] < 0 ? 0 : this.lightSelected[color];
+
+            light[color] = this.lightSelected[color];
+        },
+        changeSize()
+        {
+            let light = this.setup.lines[this.lightSelected.line].lights[this.lightSelected.light];
+            this.lightSelected.size = this.lightSelected.size.replace(" ","");
+            this.lightSelected.size = this.lightSelected.size.replace(/[^0-9]/g,"");
+
+            light.size = this.lightSelected.size;
+        },
         debug()
         {
             console.log(`Light itensity: ${this.ligths[0].itensity}`);
@@ -141,7 +161,15 @@ const vm_main = new Vue({
         }
     },
     watch: {
-        
+        speed(){
+            this.speed = this.speed.replace(" ","");
+            this.speed = this.speed.replace(/[^0-9]/g,"");
+        },
+        maxItensity(){
+            this.maxItensity = this.maxItensity.toString().replace(" ","");
+            this.maxItensity = this.maxItensity.toString().replace(/[^0-9]/g,"");
+            this.maxItensity = this.maxItensity > 100 ? 100 : this.maxItensity;
+        }
     },
     created() {
         this.updateFrame();
